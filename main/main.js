aa661d778b708254740024da4a48b6da92435e01
zonghanswjtu <qazxsw87334348@gmail.com>
2019-05-30 13:24:42 +0800

    good

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+    var line1 = [   '._.',   '...',   '._.',   '._.',   '...',   '._.',   '._.',   '._.',   '._.',   '._.' ];
+    var line2 = [   '|.|',   '..|',   '._|',   '._|',   '|_|',   '|_.',   '|_.',   '..|',   '|_|',   '|_|' ];
+    var line3 = [   '|_|',   '..|',   '|_.',   '._|',   '..|',   '._|',   '|_|',   '..|',   '|_|',   '..|' ];
+    var out1 = [], out2 = [], out3 = [];
+    var s = num.slice();
+    for(var n in s) {
+        out1.push(line1[s[n]]);
+        out2.push(line2[s[n]]);
+        out3.push(line3[s[n]]);
+    }
+    var ans = out1.join(' ') + '\n' + out2.join(' ') + '\n' + out3.join(' ') + '\n';
+    console.log(ans);
+    return ans;
 };
